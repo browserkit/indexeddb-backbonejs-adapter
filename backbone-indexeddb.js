b430f1e731df3e06203027304e30aa752b5d6598
Anjey Tsibylskij <anjey.tsibylskij@gmail.com>
2016-03-09 15:55:43 +0200

    Set name AMD module

--- backbone-indexeddb.js
+++ backbone-indexeddb.js
@@ -1,7 +1,6 @@
 (function (root, factory) {
     if (typeof define === 'function' && define.amd) {
-        // AMD. Register as an anonymous module.
-        define(['backbone', 'underscore'], factory);
+        define('backbone_indexeddb', ['backbone', 'underscore'], factory);
     } else if (typeof exports === 'object') {
         // Node. Does not work with strict CommonJS, but
         // only CommonJS-like environments that support module.exports,
